72230d2aaa6fb5db7b947b701c2cdcb864296a23
krolew <maks2iwan@gmail.com>
2024-03-25 18:03:39 +0100

    Update files

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,55 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    bundle: path.resolve(__dirname, "src/js/index.js"),
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name][contenthash].js",
-    clean: true,
-    assetModuleFilename: "[name][ext]",
-  },
-  devtool: "source-map",
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, "src/js/index.js"),
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name][contenthash].js",
+        clean: true,
+        assetModuleFilename: "[name][ext]",
+    },
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
             },
-          },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
         ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Weather App",
+            filename: "index.html",
+            template: "src/template.html",
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Weather App",
-      filename: "index.html",
-      template: "src/template.html",
-    }),
-  ],
 };
