b9cb30ace880bcf16fd4513b79adf40e3825b080
krolew <maks2iwan@gmail.com>
2024-03-29 17:52:05 +0100

    Remove and update code

--- src/js/api.js
+++ src/js/api.js
@@ -1,64 +1,36 @@
 import Util from "../js/util.js"
+import Ui from "../js/ui.js";
 
 const Api = (() => {
-    async function getWeather(location, apiKey="ee6b24dae6978abc8d8d4fbb69868568", units="metric") {
-
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${units}`;
+    async function getWeathers(location, apiKey ="ee6b24dae6978abc8d8d4fbb69868568", units = "metric") {
+        const urls = [
+            `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${units}`,
+            `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=${units}`
+        ]
 
         try {
-            const response = await fetch(apiUrl, {mode: "cors"});
+            const [currentWeather, fiveDayWeather] = await fetchWeather(urls);
+            return [currentWeather, fiveDayWeather];
 
-            if (!response.ok) {
-                const message = "Location not found";
-                return new Error(message);
-            }
-
-            const weatherJson = await response.json();
-
-            return weatherJson;
         } catch (err) {
-            throw err;
+            console.log(err);
         }
     }
 
-    async function getFiveDayWeather(location, apiKey ="ee6b24dae6978abc8d8d4fbb69868568", units = "metric") {
-
-        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=${units}`;
-
-        try {
-            const response = await fetch(apiUrl, {mode: "cors"});
-
-            if (!response.ok) {
-                const message = "Location not found";
-                return new Error(message);
-            }
-
-            const weathers = await response.json();
-
-            return Util.extractFiveDaysWeather(weathers.list);
-        } catch (err) {
-            throw err;
-        }
+    async function fetchWeather(urls) {
+        return Promise.all(
+            urls.map(url => fetch(url, {mode: "cors"})
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json();
+                    }
+                })
+                .catch(error => error)
+            )
+        )
     }
-
-    async function getImage(imgName) {
-        const apiImg = `https://openweathermap.org/img/wn/${imgName}@2x.png`
-
-        try {
-            const response = await fetch(apiImg);
-            const imageBlob = await response.blob();
-            const imgUrl = URL.createObjectURL(imageBlob);
-
-            return imgUrl;
-        } catch (err) {
-            return "";
-        }
-    }
-
     return {
-        getWeather,
-        getFiveDayWeather,
-        getImage,
+        getWeathers,
     }
 
 })()
